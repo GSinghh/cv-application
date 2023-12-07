650a9ef942f951fce5c0099393139d38b17fcaf0
GSinghh <gbassans@yahoo.com>
2023-12-06 23:28:39 -0800

    Added UserData Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import '../styles/App.css'
 import '../components/Header.jsx'
 import Header from '../components/Header.jsx'
+import UserData from './UserData.jsx'
 
 const App = () => {
   return (
     <div className = "container">
       <div className = "left-side">
         <Header />
+        <UserData />
       </div>
       <div className = "right-side">
       </div>
