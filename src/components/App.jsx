963395e71ac4802d0a1e645085e8fe200e7b7c14
GSinghh <gbassans@yahoo.com>
2023-12-12 01:03:58 -0800

    Added state to parent component for child components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,20 @@ import Header from '../components/Header.jsx'
 import UserData from './UserData.jsx'
 import Education from './Education.jsx'
 import Experience from './Experience.jsx'
+import { useState } from 'react'
 
 const App = () => {
+  const [userData, setUserData] = useState({});
+  const [educationData, setEducationData] = useState({});
+  const [experienceData, setExperienceData] = useState({});
+  
   return (
     <div className = "container">
       <div className = "left-side">
         <Header />
-        <UserData />
-        <Education />
-        <Experience />
+        <UserData userData = {userData} setUserData = {setUserData}/>
+        <Education educationData = {educationData} setEducationData = {setEducationData}/>
+        <Experience experienceData = {experienceData} setExperienceData = {setExperienceData}/>
       </div>
       <div className = "right-side">
       </div>
