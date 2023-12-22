97ff3dc7023535c14857e422707160a23155ff02
GSinghh <gbassans@yahoo.com>
2023-12-21 23:48:15 -0800

    Finished resume header

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,6 +4,11 @@ const Resume = ({ userData, expData, eduData }) => {
     return (
         <div className = 'resume-container'>
             <h1 className = 'resume-title'>{userData.fullName}</h1>
+            <div className = 'resume-header'>
+                <h3>{userData.email}</h3> 
+                <h3>{userData.phoneNumber}</h3>
+                <h3>{userData.address}</h3>
+            </div>
         </div>
     );
 }
