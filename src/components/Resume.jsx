1e167ec00492e38e709a6275cb288079e4d25f81
GSinghh <gbassans@yahoo.com>
2023-12-22 19:42:57 -0800

    Added in all state data

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,6 +9,20 @@ const Resume = ({ userData, expData, eduData }) => {
                 <h3>{userData.phoneNumber}</h3>
                 <h3>{userData.address}</h3>
             </div>
+            <div className = 'edu-container'>
+                <h3>{eduData.school}</h3>
+                <h3>{eduData.degree}</h3>
+                <h3>{eduData.startDate}</h3>
+                <h3>{eduData.endDate}</h3>
+                <h3>{eduData.location}</h3>
+            </div>
+            <div className = 'work-container'>
+                <h3>{expData.position}</h3>
+                <h3>{expData.company}</h3>
+                <h3>{expData.startDate}</h3>
+                <h3>{expData.endDate}</h3>
+                <h3>{expData.description}</h3>
+            </div>
         </div>
     );
 }
