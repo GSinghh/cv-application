3f86cf1b65ceafc0bb783e20d79ac53f5e4a7c7b
GSinghh <gbassans@yahoo.com>
2023-12-23 23:59:12 -0800

    Fixed formatting

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,9 @@ import '../styles/Resume.css'
 const Resume = ({ userData, expData, eduData }) => {
     return (
         <div className = 'resume-container'>
-            <h1 className = 'resume-title'>{userData.fullName}</h1>
+            <h1 className = 'resume-title'>
+                {userData.fullName}
+            </h1>
             <div className = 'resume-header'>
                 <h3>{userData.email}</h3> 
                 <h3>{userData.phoneNumber}</h3>
