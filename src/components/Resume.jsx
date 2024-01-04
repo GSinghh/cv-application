2202b46ddbf45cf48d613ae92b46ab85b8139207
GSinghh <gbassans@yahoo.com>
2024-01-03 23:53:29 -0800

    Added anchor tags for links

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,8 @@
 import '../styles/Resume.css'
 
 const Resume = ({ userData, expData, eduData }) => {
+    const githubLink = "https://www." + userData.github;
+    const linkedinLink = "https://www." + userData.linkedin;
     return (
         <div className = 'resume-container'>
             <h1 className = 'resume-title'>
@@ -8,17 +10,26 @@ const Resume = ({ userData, expData, eduData }) => {
             </h1>
             <div className = 'resume-header'>
                 <h3>{userData.email}</h3> 
-                <h3>{userData.github}</h3>
-                <h3>{userData.linkedin}</h3>
+                <a href = {githubLink} target = "_blank">
+                    <h3>{userData.github}</h3>   
+                </a>
+                <a href = {linkedinLink} target = "_blank">
+                    <h3>{userData.linkedin}</h3>
+                </a>
             </div>
-            <div className = 'edu-container'>
+            <div className = 'education-container'>
+                <h2>Education</h2>
+                <div className = 'line'></div>
+
                 <h3>{eduData.school}</h3>
                 <h3>{eduData.degree}</h3>
                 <h3>{eduData.startDate}</h3>
                 <h3>{eduData.endDate}</h3>
                 <h3>{eduData.location}</h3>
             </div>
-            <div className = 'work-container'>
+            <div className = 'workexp-container'>
+                <h2>Work Experience</h2>
+                <div className = "line"></div>
                 <h3>{expData.position}</h3>
                 <h3>{expData.company}</h3>
                 <h3>{expData.startDate}</h3>
