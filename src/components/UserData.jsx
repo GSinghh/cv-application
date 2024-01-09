962e5411586067091244d616a162c5df92ebfa02
GSinghh <gbassans@yahoo.com>
2024-01-08 23:54:29 -0800

    Added in placeholder for form elements

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -12,13 +12,13 @@ const UserData = ({ userData, setUserData }) => {
             <h1 className = "user-title">Personal Information</h1>
             <form className = "user-form">
                 <label>Full Name</label>
-                <input type = "text" className = "user-input" value = {userData.fullName} onChange={handleInputChange} name = "fullName"/>
+                <input type = "text" className = "user-input" value = {userData.fullName} onChange={handleInputChange} name = "fullName" placeholder = 'i.e. John Doe' />
                 <label>Email</label>
-                <input type = "text" className = "user-input" value = {userData.email} onChange={handleInputChange} name = "email"/>
+                <input type = "text" className = "user-input" value = {userData.email} onChange={handleInputChange} name = "email" placeholder = 'i.e. email@domain.com' />
                 <label>Github</label>
-                <input type = "text" className = "user-input" value = {userData.github} onChange={handleInputChange} name = "github"/>
+                <input type = "text" className = "user-input" value = {userData.github} onChange={handleInputChange} name = "github" placeholder='i.e. github.com/username'/>
                 <label>Linkedin</label>
-                <input type = "text" className = "user-input"value = {userData.linkedin} onChange={handleInputChange} name = "linkedin"/>
+                <input type = "text" className = "user-input"value = {userData.linkedin} onChange={handleInputChange} name = "linkedin" placeholder='i.e. linkedin.com/in/linkedin-username' />
             </form>
         </div>
     );
