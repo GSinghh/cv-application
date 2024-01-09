d180a50f63447d4003fc894a7c037b8e6c462382
GSinghh <gbassans@yahoo.com>
2024-01-08 23:53:46 -0800

    Added in state for skills component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Header from '../components/Header.jsx'
 import UserData from './UserData.jsx'
 import Education from './Education.jsx'
 import Experience from './Experience.jsx'
+import Skills from './Skills.jsx'
 import Resume from './Resume.jsx'
 import { useState } from 'react'
 
@@ -11,6 +12,7 @@ const App = () => {
   const [userData, setUserData] = useState({});
   const [educationData, setEducationData] = useState({});
   const [experienceData, setExperienceData] = useState({});
+  const [skillsData, setSkillsData] = useState({});
 
   return (
     <div className = "container">
@@ -19,9 +21,10 @@ const App = () => {
         <UserData userData = {userData} setUserData = {setUserData}/>
         <Education educationData = {educationData} setEducationData = {setEducationData}/>
         <Experience experienceData = {experienceData} setExperienceData = {setExperienceData}/>
+        <Skills skillsData = {skillsData} setSkillsData = {setSkillsData}/> 
       </div>
       <div className = "right-side">
-        <Resume eduData = {educationData} userData = {userData} expData = {experienceData} />
+        <Resume eduData = {educationData} userData = {userData} expData = {experienceData} skillsData = {skillsData} />
       </div>
     </div> 
   )
