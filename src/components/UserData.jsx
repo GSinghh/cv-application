3a2fddacc015fd1f60c101f2d28bcbf6ca7fb905
GSinghh <gbassans@yahoo.com>
2024-01-12 03:05:25 -0800

    added collapsability to form component

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -1,55 +1,87 @@
 import "../styles/UserData.css";
+import arrowUp from "../assets/arrowUp.svg";
 import { useState } from "react";
 
 const UserData = ({ userData, setUserData }) => {
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserData({ ...userData, [name]: value });
-  };
+    const [isOpen, setIsOpen] = useState(true);
+    const [isClosing, setIsClosing] = useState(false);
+    const [rotateArrow, setRotateArrow] = useState(false);
 
-  return (
-    <div className="user-container">
-      <h1 className="user-title">Personal Information</h1>
-      <form className="user-form">
-        <label>Full Name</label>
-        <input
-          type="text"
-          className="user-input"
-          value={userData.fullName}
-          onChange={handleInputChange}
-          name="fullName"
-          placeholder="i.e. John Doe"
-        />
-        <label>Email</label>
-        <input
-          type="text"
-          className="user-input"
-          value={userData.email}
-          onChange={handleInputChange}
-          name="email"
-          placeholder="i.e. email@domain.com"
-        />
-        <label>Github</label>
-        <input
-          type="text"
-          className="user-input"
-          value={userData.github}
-          onChange={handleInputChange}
-          name="github"
-          placeholder="i.e. github.com/username"
-        />
-        <label>Linkedin</label>
-        <input
-          type="text"
-          className="user-input"
-          value={userData.linkedin}
-          onChange={handleInputChange}
-          name="linkedin"
-          placeholder="i.e. linkedin.com/in/linkedin-username"
-        />
-      </form>
-    </div>
-  );
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserData({ ...userData, [name]: value });
+    };
+
+    const toggleAccordion = () => {
+        setIsClosing(true);
+        setTimeout(() => {
+            setIsOpen(!isOpen);
+            setIsClosing(false);
+            setRotateArrow(!rotateArrow);
+        }, 500);
+    };
+
+    return (
+        <div
+            className={`user-container ${isOpen ? "open" : ""}
+            ${isClosing ? "closing" : ""}`}
+        >
+            <div className="user-header">
+                <h1 className="user-title">Personal Information</h1>
+                <div className="arrow" onClick={toggleAccordion}>
+                    <img
+                        src={arrowUp}
+                        alt="arrow"
+                        style={{
+                            transform: rotateArrow
+                                ? "rotate(180deg)"
+                                : "rotate(0)",
+                        }}
+                    />
+                </div>
+            </div>
+            {isOpen && (
+                <form className="user-form">
+                    <label>Full Name</label>
+                    <input
+                        type="text"
+                        className="user-input"
+                        value={userData.fullName}
+                        onChange={handleInputChange}
+                        name="fullName"
+                        placeholder="i.e. John Doe"
+                    />
+                    <label>Email</label>
+                    <input
+                        type="text"
+                        className="user-input"
+                        value={userData.email}
+                        onChange={handleInputChange}
+                        name="email"
+                        placeholder="i.e. email@domain.com"
+                    />
+                    <label>Github</label>
+                    <input
+                        type="text"
+                        className="user-input"
+                        value={userData.github}
+                        onChange={handleInputChange}
+                        name="github"
+                        placeholder="i.e. github.com/username"
+                    />
+                    <label>Linkedin</label>
+                    <input
+                        type="text"
+                        className="user-input"
+                        value={userData.linkedin}
+                        onChange={handleInputChange}
+                        name="linkedin"
+                        placeholder="i.e. linkedin.com/in/linkedin-username"
+                    />
+                </form>
+            )}
+        </div>
+    );
 };
 
 export default UserData;
