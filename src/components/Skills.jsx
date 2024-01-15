501579f66627447351f885be80d80781d8b7df75
GSinghh <gbassans@yahoo.com>
2024-01-14 23:43:18 -0800

    Added state variables for animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,46 +1,51 @@
 import "../styles/Skills.css";
 import { useState } from "react";
+import arrowUp from "../assets/arrowUp.svg";
 
 const Skills = ({ skillsData, setSkillsData }) => {
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setSkillsData({ ...skillsData, [name]: value });
-  };
+    const [isOpen, setIsOpen] = useState(true);
+    const [isClosing, setIsClosing] = useState(false);
+    const [rotateArrow, setRotateArrow] = useState(false);
 
-  return (
-    <div className="skills-container">
-      <h1 className="skills-title">Skills</h1>
-      <form className="skills-form">
-        <label>Languages</label>
-        <input
-          type="text"
-          className="skills-input"
-          value={skillsData.languages}
-          onChange={handleInputChange}
-          name="languages"
-          placeholder="i.e. Javascript, Python, C++"
-        />
-        <label>Technologies</label>
-        <input
-          type="text"
-          className="skills-input"
-          value={skillsData.technologies}
-          onChange={handleInputChange}
-          name="technologies"
-          placeholder="i.e. React.js, Express.js, Node.js"
-        />
-        <label>Tools</label>
-        <input
-          type="text"
-          className="skills-input"
-          value={skillsData.tools}
-          onChange={handleInputChange}
-          name="tools"
-          placeholder="i.e. Git, MongoDB, Vite"
-        />
-      </form>
-    </div>
-  );
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setSkillsData({ ...skillsData, [name]: value });
+    };
+
+    return (
+        <div className="skills-container">
+            <h1 className="skills-title">Skills</h1>
+            <form className="skills-form">
+                <label>Languages</label>
+                <input
+                    type="text"
+                    className="skills-input"
+                    value={skillsData.languages}
+                    onChange={handleInputChange}
+                    name="languages"
+                    placeholder="i.e. Javascript, Python, C++"
+                />
+                <label>Technologies</label>
+                <input
+                    type="text"
+                    className="skills-input"
+                    value={skillsData.technologies}
+                    onChange={handleInputChange}
+                    name="technologies"
+                    placeholder="i.e. React.js, Express.js, Node.js"
+                />
+                <label>Tools</label>
+                <input
+                    type="text"
+                    className="skills-input"
+                    value={skillsData.tools}
+                    onChange={handleInputChange}
+                    name="tools"
+                    placeholder="i.e. Git, MongoDB, Vite"
+                />
+            </form>
+        </div>
+    );
 };
 
 export default Skills;
