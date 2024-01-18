307d51b132a2d93c1485a724ebc6312683e27922
GSinghh <gbassans@yahoo.com>
2024-01-18 03:52:07 -0800

    replaced auxiliary code with Components

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,74 +1,25 @@
 import "../styles/Resume.css";
+import ResumeHeader from "../components/Resume/Header/ResumeHeader.jsx";
+import LineHeader from "./Resume/Utilities/LineHeader.jsx";
+import EducationContent from "./Resume/Education/EducationContent.jsx";
+import ExperienceContent from "./Resume/Experience/ExperienceContent.jsx";
+import SkillsContent from "./Resume/Skills/SkillsContent.jsx";
 
 const Resume = ({ userData, expData, eduData, skillsData }) => {
-    const githubLink = "https://www." + userData.github;
-    const linkedinLink = "https://www." + userData.linkedin;
     return (
         <div className="resume-container">
-            <h1 className="resume-title">{userData.fullName}</h1>
-            <div className="resume-header">
-                <h3>{userData.email}</h3>
-                <a href={githubLink} target="_blank">
-                    <h3>{userData.github}</h3>
-                </a>
-                <a href={linkedinLink} target="_blank">
-                    <h3>{userData.linkedin}</h3>
-                </a>
-            </div>
+            <ResumeHeader userData={userData} />
             <div className="education-container">
-                <div className="education-header">
-                    <h2>Education</h2>
-                    <div className="line"></div>
-                </div>
-                <div className="content-body">
-                    <div className="first">
-                        <h3>{eduData.school}</h3>
-                        {eduData.startDate && eduData.endDate && (
-                            <h3>
-                                {eduData.startDate + " - " + eduData.endDate}
-                            </h3>
-                        )}
-                    </div>
-                    <div className="second">
-                        <h3>{eduData.degree}</h3>
-                        <h3>{eduData.location}</h3>
-                    </div>
-                </div>
+                <LineHeader title={"Education"} />
+                <EducationContent eduData={eduData} />
             </div>
             <div className="workexp-container">
-                <div className="workexp-header">
-                    <h2>Work Experience</h2>
-                    <div className="line"></div>
-                </div>
-                <div className="content-body">
-                    <div className="first">
-                        <h3>{expData.position}</h3>
-                        {expData.startDate && expData.endDate && (
-                            <h3>
-                                {expData.startDate + " - " + expData.endDate}
-                            </h3>
-                        )}
-                    </div>
-                    <div className="second">
-                        <h3>{expData.company}</h3>
-                    </div>
-                </div>
-                <h3>{expData.description}</h3>
+                <LineHeader title={"Work Experience"} />
+                <ExperienceContent expData={expData} />
             </div>
-            <div classNames="skills-resume-container">
-                <div className="skills-resume-header">
-                    <h2>Skills</h2>
-                    <div className="line"></div>
-                </div>
-                <div className="content-body">
-                    {skillsData.languages && (
-                        <h3>Languages: {skillsData.languages}</h3>
-                    )}
-                    {skillsData.technologies && (
-                        <h3>Technologies: {skillsData.technologies}</h3>
-                    )}
-                    {skillsData.tools && <h3>Tools: {skillsData.tools}</h3>}
-                </div>
+            <div classNames="skills-content-container">
+                <LineHeader title={"Skills"} />
+                <SkillsContent skillsData={skillsData} />
             </div>
         </div>
     );
