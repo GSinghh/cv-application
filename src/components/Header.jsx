d55f5d076a612205bee7a13fada7a59269a73965
GSinghh <gbassans@yahoo.com>
2024-01-29 23:24:05 -0800

    Made button functional to download PDF

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,45 @@
 import "../styles/Header.css";
 import downloadArrow from "../assets/downloadArrow.svg";
 import githubMark from "../assets/github-mark.svg";
+import ResumePDF from "../components/Resume/Utilities/ResumePDF";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+
 const Header = () => {
     return (
         <div className="header-container">
             <div className="title-container">
                 <h1 className="title">Resume.IO</h1>
-                <button type="button" className="button">
+                <PDFDownloadLink document={<ResumePDF />} fileName="Resume.pdf">
+                    {({ blob, url, loading, error }) =>
+                        loading ? (
+                            <button className="button" type="button">
+                                Loading
+                                <img
+                                    className="button-icon"
+                                    src={downloadArrow}
+                                    alt="download icon"
+                                />
+                            </button>
+                        ) : (
+                            <button className="button">
+                                Download
+                                <img
+                                    className="button-icon"
+                                    src={downloadArrow}
+                                    alt="download icon"
+                                />
+                            </button>
+                        )
+                    }
+                </PDFDownloadLink>
+                {/* <button type="button" className="button">
                     <span className="button-text">Download</span>
                     <img
                         className="button-icon"
                         src={downloadArrow}
                         alt="download icon"
                     />
-                </button>
+                </button> */}
             </div>
             <div className="footer">
                 <a href="https://github.com/GSinghh">
